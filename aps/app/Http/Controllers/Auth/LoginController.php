fa6772bfab658ce724dc95991d6022c899013d88
er1an9 <sun_eagle@ymail.com>
2017-07-07 05:31:00 +0700

    use DB

--- aps/app/Http/Controllers/Auth/LoginController.php
+++ aps/app/Http/Controllers/Auth/LoginController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Support\Facades\DB;
 
 class LoginController extends Controller
 {
@@ -35,6 +36,7 @@ class LoginController extends Controller
     public function __construct()
     {
         $this->middleware('guest', ['except' => 'logout']);
+        $this->data['abouts'] = DB::table('about')->first();
     }
 
     public function username()
